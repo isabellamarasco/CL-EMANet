b6c2370f6d0ae19bde7ffd6018a12c2ceea45b7e
Davide Evangelista <davide.evangelista5@unibo.it>
2025-07-16 20:00:09 +0200

    Added experiments

--- experiments/CICIDS.sh
+++ experiments/CICIDS.sh
@@ -1,122 +1,17 @@
 #!/bin/bash
 set -e
 
-python trainContinuousFlow.py \
-    --data_name CIC-IDS \
-    --continuous_flow_type daily \
-    --normalization_type no \
-    --buffer_type no \
-    --batch_size 20000 \
-    --n_epochs 20 \
-    --learning_rate 5e-4 \
-    --eta 0.99
+python trainContinuousFlow.py --data_name CIC-IDS --continuous_flow_type daily --normalization_type no --buffer_type no --batch_size 20000 --n_epochs 20 --learning_rate 5e-4 --eta 0.99
+python trainContinuousFlow.py --data_name CIC-IDS --continuous_flow_type daily --normalization_type global --buffer_type no --batch_size 20000 --n_epochs 20 --learning_rate 5e-4 --eta 0.99
+python trainContinuousFlow.py --data_name CIC-IDS --continuous_flow_type daily --normalization_type local --buffer_type no --batch_size 20000 --n_epochs 20 --learning_rate 5e-4 --eta 0.99
+python trainContinuousFlow.py --data_name CIC-IDS --continuous_flow_type daily --normalization_type EMANet --buffer_type no --batch_size 20000 --n_epochs 20 --learning_rate 5e-4 --eta 0.99
 
-python trainContinuousFlow.py \
-    --data_name CIC-IDS \
-    --continuous_flow_type daily \
-    --normalization_type global \
-    --buffer_type no \
-    --batch_size 20000 \
-    --n_epochs 20 \
-    --learning_rate 5e-4 \
-    --eta 0.99
+python trainContinuousFlow.py --data_name CIC-IDS --continuous_flow_type daily --normalization_type no --buffer_type random --batch_size 20000 --n_epochs 20 --learning_rate 5e-4 --eta 0.99
+python trainContinuousFlow.py --data_name CIC-IDS --continuous_flow_type daily --normalization_type global --buffer_type random --batch_size 20000 --n_epochs 20 --learning_rate 5e-4 --eta 0.99
+python trainContinuousFlow.py --data_name CIC-IDS --continuous_flow_type daily --normalization_type local --buffer_type random --batch_size 20000 --n_epochs 20 --learning_rate 5e-4 --eta 0.99
+python trainContinuousFlow.py --data_name CIC-IDS --continuous_flow_type daily --normalization_type EMANet --buffer_type random --batch_size 20000 --n_epochs 20 --learning_rate 5e-4 --eta 0.99
 
-python trainContinuousFlow.py \
-    --data_name CIC-IDS \
-    --continuous_flow_type daily \
-    --normalization_type local \
-    --buffer_type no \
-    --batch_size 20000 \
-    --n_epochs 20 \
-    --learning_rate 5e-4 \
-    --eta 0.99
-
-python trainContinuousFlow.py \
-    --data_name CIC-IDS \
-    --continuous_flow_type daily \
-    --normalization_type EMANet \
-    --buffer_type no \
-    --batch_size 20000 \
-    --n_epochs 20 \
-    --learning_rate 5e-4 \
-    --eta 0.99
-
-python trainContinuousFlow.py \
-    --data_name CIC-IDS \
-    --continuous_flow_type daily \
-    --normalization_type no \
-    --buffer_type random \
-    --batch_size 20000 \
-    --n_epochs 20 \
-    --learning_rate 5e-4 \
-    --eta 0.99
-
-python trainContinuousFlow.py \
-    --data_name CIC-IDS \
-    --continuous_flow_type daily \
-    --normalization_type global \
-    --buffer_type random \
-    --batch_size 20000 \
-    --n_epochs 20 \
-    --learning_rate 5e-4 \
-    --eta 0.99
-
-python trainContinuousFlow.py \
-    --data_name CIC-IDS \
-    --continuous_flow_type daily \
-    --normalization_type local \
-    --buffer_type random \
-    --batch_size 20000 \
-    --n_epochs 20 \
-    --learning_rate 5e-4 \
-    --eta 0.99
-
-python trainContinuousFlow.py \
-    --data_name CIC-IDS \
-    --continuous_flow_type daily \
-    --normalization_type EMANet \
-    --buffer_type random \
-    --batch_size 20000 \
-    --n_epochs 20 \
-    --learning_rate 5e-4 \
-    --eta 0.99
-
-python trainContinuousFlow.py \
-    --data_name CIC-IDS \
-    --continuous_flow_type daily \
-    --normalization_type no \
-    --buffer_type agem \
-    --batch_size 20000 \
-    --n_epochs 20 \
-    --learning_rate 5e-4 \
-    --eta 0.99
-
-python trainContinuousFlow.py \
-    --data_name CIC-IDS \
-    --continuous_flow_type daily \
-    --normalization_type global \
-    --buffer_type agem \
-    --batch_size 20000 \
-    --n_epochs 20 \
-    --learning_rate 5e-4 \
-    --eta 0.99
-
-python trainContinuousFlow.py \
-    --data_name CIC-IDS \
-    --continuous_flow_type daily \
-    --normalization_type local \
-    --buffer_type agem \
-    --batch_size 20000 \
-    --n_epochs 20 \
-    --learning_rate 5e-4 \
-    --eta 0.99
-
-python trainContinuousFlow.py \
-    --data_name CIC-IDS \
-    --continuous_flow_type daily \
-    --normalization_type EMANet \
-    --buffer_type agem \
-    --batch_size 20000 \
-    --n_epochs 20 \
-    --learning_rate 5e-4 \
-    --eta 0.99
+python trainContinuousFlow.py --data_name CIC-IDS --continuous_flow_type daily --normalization_type no --buffer_type agem --batch_size 20000 --n_epochs 20 --learning_rate 5e-4 --eta 0.99
+python trainContinuousFlow.py --data_name CIC-IDS --continuous_flow_type daily --normalization_type global --buffer_type agem --batch_size 20000 --n_epochs 20 --learning_rate 5e-4 --eta 0.99
+python trainContinuousFlow.py --data_name CIC-IDS --continuous_flow_type daily --normalization_type local --buffer_type agem --batch_size 20000 --n_epochs 20 --learning_rate 5e-4 --eta 0.99
+python trainContinuousFlow.py --data_name CIC-IDS --continuous_flow_type daily --normalization_type EMANet --buffer_type agem --batch_size 20000 --n_epochs 20 --learning_rate 5e-4 --eta 0.99
--- experiments/UNSW-NB15.sh
+++ experiments/UNSW-NB15.sh
@@ -1,121 +1,17 @@
 #!/bin/bash
+set -e
 
-python trainContinuousFlow.py \
-    --data_name USNW-NB15 \
-    --continuous_flow_type daily \
-    --normalization_type no \
-    --buffer_type no \
-    --batch_size 20000 \
-    --n_epochs 20 \
-    --learning_rate 5e-4 \
-    --eta 0.99
+python trainContinuousFlow.py --data_name UNSW-NB15 --continuous_flow_type daily --normalization_type no --buffer_type no --batch_size 20000 --n_epochs 20 --learning_rate 5e-4 --eta 0.99
+python trainContinuousFlow.py --data_name UNSW-NB15 --continuous_flow_type daily --normalization_type global --buffer_type no --batch_size 20000 --n_epochs 20 --learning_rate 5e-4 --eta 0.99
+python trainContinuousFlow.py --data_name UNSW-NB15 --continuous_flow_type daily --normalization_type local --buffer_type no --batch_size 20000 --n_epochs 20 --learning_rate 5e-4 --eta 0.99
+python trainContinuousFlow.py --data_name UNSW-NB15 --continuous_flow_type daily --normalization_type EMANet --buffer_type no --batch_size 20000 --n_epochs 20 --learning_rate 5e-4 --eta 0.99
 
-python trainContinuousFlow.py \
-    --data_name USNW-NB15 \
-    --continuous_flow_type daily \
-    --normalization_type global \
-    --buffer_type no \
-    --batch_size 20000 \
-    --n_epochs 20 \
-    --learning_rate 5e-4 \
-    --eta 0.99
+python trainContinuousFlow.py --data_name UNSW-NB15 --continuous_flow_type daily --normalization_type no --buffer_type random --batch_size 20000 --n_epochs 20 --learning_rate 5e-4 --eta 0.99
+python trainContinuousFlow.py --data_name UNSW-NB15 --continuous_flow_type daily --normalization_type global --buffer_type random --batch_size 20000 --n_epochs 20 --learning_rate 5e-4 --eta 0.99
+python trainContinuousFlow.py --data_name UNSW-NB15 --continuous_flow_type daily --normalization_type local --buffer_type random --batch_size 20000 --n_epochs 20 --learning_rate 5e-4 --eta 0.99
+python trainContinuousFlow.py --data_name UNSW-NB15 --continuous_flow_type daily --normalization_type EMANet --buffer_type random --batch_size 20000 --n_epochs 20 --learning_rate 5e-4 --eta 0.99
 
-python trainContinuousFlow.py \
-    --data_name USNW-NB15 \
-    --continuous_flow_type daily \
-    --normalization_type local \
-    --buffer_type no \
-    --batch_size 20000 \
-    --n_epochs 20 \
-    --learning_rate 5e-4 \
-    --eta 0.99
-
-python trainContinuousFlow.py \
-    --data_name USNW-NB15 \
-    --continuous_flow_type daily \
-    --normalization_type EMANet \
-    --buffer_type no \
-    --batch_size 20000 \
-    --n_epochs 20 \
-    --learning_rate 5e-4 \
-    --eta 0.99
-
-python trainContinuousFlow.py \
-    --data_name USNW-NB15 \
-    --continuous_flow_type daily \
-    --normalization_type no \
-    --buffer_type random \
-    --batch_size 20000 \
-    --n_epochs 20 \
-    --learning_rate 5e-4 \
-    --eta 0.99
-
-python trainContinuousFlow.py \
-    --data_name USNW-NB15 \
-    --continuous_flow_type daily \
-    --normalization_type global \
-    --buffer_type random \
-    --batch_size 20000 \
-    --n_epochs 20 \
-    --learning_rate 5e-4 \
-    --eta 0.99
-
-python trainContinuousFlow.py \
-    --data_name USNW-NB15 \
-    --continuous_flow_type daily \
-    --normalization_type local \
-    --buffer_type random \
-    --batch_size 20000 \
-    --n_epochs 20 \
-    --learning_rate 5e-4 \
-    --eta 0.99
-
-python trainContinuousFlow.py \
-    --data_name USNW-NB15 \
-    --continuous_flow_type daily \
-    --normalization_type EMANet \
-    --buffer_type random \
-    --batch_size 20000 \
-    --n_epochs 20 \
-    --learning_rate 5e-4 \
-    --eta 0.99
-
-python trainContinuousFlow.py \
-    --data_name USNW-NB15 \
-    --continuous_flow_type daily \
-    --normalization_type no \
-    --buffer_type agem \
-    --batch_size 20000 \
-    --n_epochs 20 \
-    --learning_rate 5e-4 \
-    --eta 0.99
-
-python trainContinuousFlow.py \
-    --data_name USNW-NB15 \
-    --continuous_flow_type daily \
-    --normalization_type global \
-    --buffer_type agem \
-    --batch_size 20000 \
-    --n_epochs 20 \
-    --learning_rate 5e-4 \
-    --eta 0.99
-
-python trainContinuousFlow.py \
-    --data_name USNW-NB15 \
-    --continuous_flow_type daily \
-    --normalization_type local \
-    --buffer_type agem \
-    --batch_size 20000 \
-    --n_epochs 20 \
-    --learning_rate 5e-4 \
-    --eta 0.99
-
-python trainContinuousFlow.py \
-    --data_name USNW-NB15 \
-    --continuous_flow_type daily \
-    --normalization_type EMANet \
-    --buffer_type agem \
-    --batch_size 20000 \
-    --n_epochs 20 \
-    --learning_rate 5e-4 \
-    --eta 0.99
+python trainContinuousFlow.py --data_name UNSW-NB15 --continuous_flow_type daily --normalization_type no --buffer_type agem --batch_size 20000 --n_epochs 20 --learning_rate 5e-4 --eta 0.99
+python trainContinuousFlow.py --data_name UNSW-NB15 --continuous_flow_type daily --normalization_type global --buffer_type agem --batch_size 20000 --n_epochs 20 --learning_rate 5e-4 --eta 0.99
+python trainContinuousFlow.py --data_name UNSW-NB15 --continuous_flow_type daily --normalization_type local --buffer_type agem --batch_size 20000 --n_epochs 20 --learning_rate 5e-4 --eta 0.99
+python trainContinuousFlow.py --data_name UNSW-NB15 --continuous_flow_type daily --normalization_type EMANet --buffer_type agem --batch_size 20000 --n_epochs 20 --learning_rate 5e-4 --eta 0.99
